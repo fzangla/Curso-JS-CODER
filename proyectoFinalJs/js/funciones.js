53e3b4b7f51a17d43901d96a84d9d61b9bc22734
Franco Zangla Benitez <francomzangla@gmail.com>
2022-02-24 23:09:37 -0300

    Entrega desafio: Incorporando librerías

--- proyectoFinalJs/js/funciones.js
+++ proyectoFinalJs/js/funciones.js
@@ -1,35 +1,33 @@
-function buscarPokemon(pokemonUsuario)
-    {
-        let resultadoFind="";
-        switch(pokemonUsuario)
-        {
-                    case "pikachu":
-                        resultadoFind=arrayPokemon.find((obj) => obj.nombre === "pikachu");
-                        break;
-                    case "squirtle":
-                        resultadoFind=arrayPokemon.find((obj) => obj.nombre === "squirtle");
-                        break;
-                    case "charmander":
-                        resultadoFind=arrayPokemon.find((obj) => obj.nombre === "charmander");
-                        break;
-                    case "bulbasaur":
-                        resultadoFind=arrayPokemon.find((obj) => obj.nombre === "bulbasaur");
-                        break;
-                    default:
-                        console.log("No reconocemos ese pokemón.");
-                        break;
-        }
-        return resultadoFind;
-    }
+function buscarPokemon(pokemonUsuario) {
+  let resultadoFind = "";
+  switch (pokemonUsuario) {
+    case "pikachu":
+      resultadoFind = arrayPokemon.find((obj) => obj.nombre === "pikachu");
+      break;
+    case "squirtle":
+      resultadoFind = arrayPokemon.find((obj) => obj.nombre === "squirtle");
+      break;
+    case "charmander":
+      resultadoFind = arrayPokemon.find((obj) => obj.nombre === "charmander");
+      break;
+    case "bulbasaur":
+      resultadoFind = arrayPokemon.find((obj) => obj.nombre === "bulbasaur");
+      break;
+    default:
+      console.log("No reconocemos ese pokemón.");
+      break;
+  }
+  return resultadoFind;
+}
 
 //listado de pokemones atrapados
-const listaBag = () =>{
-    const divBag=document.querySelector("#pokemonElegido");
-  
-    let bagHTML= ""
-      
-    for(const pokemon of BAG){
-      bagHTML +=`
+const listaBag = () => {
+  const divBag = document.querySelector("#pokemonElegido");
+
+  let bagHTML = ""
+
+  for (const pokemon of BAG) {
+    bagHTML += `
       <div class="center">
       <div class="card" style="width: 18rem;">
       <img src="img/${pokemon.nombre}.png" alt="${pokemon.nombre}" class="card-img-top" height=300px>
@@ -47,18 +45,18 @@ const listaBag = () =>{
       </div>
       </div>
       `
-    }
-    divBag.innerHTML =bagHTML;
-  
   }
-  //listado de pokemones sueltos
-  const listaDePokemones = () =>{
-    const divPokemon=document.querySelector("#pokemones");
-  
-    let listaHTML= "";
-      
-    for(const pokemon of arrayPokemon){
-      listaHTML +=` 
+  divBag.innerHTML = bagHTML;
+
+}
+//listado de pokemones sueltos
+const listaDePokemones = () => {
+  const divPokemon = document.querySelector("#pokemones");
+
+  let listaHTML = "";
+
+  for (const pokemon of arrayPokemon) {
+    listaHTML += ` 
       <div class="card w-25">
           <div class="card-body">
             <h5 class="card-title">${pokemon.id}: ${pokemon.nombre}</h5>
@@ -68,41 +66,118 @@ const listaBag = () =>{
           </div>
         </div>
       `
-      // <p class="card-text"> Tipo: ${pokemon.tipo} <br>
-      // Vida: ${pokemon.puntosDeVida} <br>
-      // Ataque: ${pokemon.ataque} <br>
-      // Defensa: ${pokemon.defensa} <br>
-      // </p>
-    }
-    divPokemon.innerHTML =listaHTML;
-  
-    eventoAñadirPokemon()
-  }  
-  
-  const eventoAñadirPokemon=()=>{
-       const botonAtraparPokemon=document.getElementsByClassName("añadirPokemon");
-        for(const boton of botonAtraparPokemon){
-          boton.onclick=añadirPokemon;
+  }
+  divPokemon.innerHTML = listaHTML;
+
+  eventoAñadirPokemon()
+
+}
+
+const eventoAñadirPokemon = () => {
+  const botonAtraparPokemon = document.getElementsByClassName("añadirPokemon");
+  for (const boton of botonAtraparPokemon) {
+    boton.onclick = añadirPokemon;
+  }
+
+}
+
+const añadirPokemon = e => {
+  const id = e.target.id;
+  const pokemon = arrayPokemon.find(p => p.id == id);
+  swal({
+    title: `Seleccionaste a ${pokemon.nombre}`,
+    text: "Una vez elegido no podrás volver atrás. ¿Desea continuar?",
+    icon: "warning",
+    buttons: true,
+    dangerMode: false,
+  })
+    .then((willDelete) => {
+      if (willDelete) {
+        swal(`Elegiste a ${pokemon.nombre} como tu pokemon inicial`, "¡Descubre sus habilidades!", "success");
+        BAG.push(pokemon)
+        updateCache();
+        listaBag();
+      } else {
+        swal("No elegiste ningún pokemon inicial", "Elija uno", "error");
       }
+    });
+}
+
+const updateCache = () => {
+  const bagJSON = JSON.stringify(BAG);
+  localStorage.setItem("bag", bagJSON);
+}
+
+const getCache = () => {
+  bagJSON = localStorage.getItem("bag");
+  if (bagJSON) {
+    BAG = JSON.parse(bagJSON);
+  }
+  listaBag();
+}
+/////////////////////////////////////////////////////////////////////////////////
+PKM_TRAINER = []
+
+const namePokemonTrainer = () => {
+
+  //Pido el genero
+  gender = prompt("Ingrese genero (m o f): ").toLocaleLowerCase()
+  
+  
+  // if(gender != "m" && gender != "f"){
+  //   Toastify({
+  //     text: "Ingreso invalido",
+  //     duration: 10000,
+  //     gravity: 'bottom',
+  //     position: 'left',
+  //     className: "toastifyError",
+  //     }).showToast();
+  // }
+
+  //valido el genero
+  while (gender != "m" && gender != "f") {
+    gender = prompt("Reingrese genero  con las letras 'm' o 'f': ").toLocaleLowerCase()
+  }
+
+  //Pido el nombre
+  trainer = prompt("Ingrese nombre: ")
+
+  //valido el nombre
+  while (trainer == "") {
+    trainer = prompt("Reingrese nombre: ")
   }
   
-  const añadirPokemon = e =>{
-    const id= e.target.id;
-    const pokemon = arrayPokemon.find(p => p.id == id);
-    BAG.push(pokemon);
-    updateCache();
-    listaBag();
-  }
+  //Guardo el nombre
+  trainerNameJSON = localStorage.setItem("name", trainer)
+
+  // declaro los mensajes 
+  const holaF = "Bienvenida entrenadora " + trainer;
+  const holaM = "Bienvenido entrenador " + trainer;
   
-  const updateCache = () =>{
-    const bagJSON= JSON.stringify(BAG);
-    localStorage.setItem("bag", bagJSON);
+  // con el ternario saludo a quien corresponda
+  gender == "m" ? swal(holaM) : swal(holaF);
+
+  return trainerNameJSON;
+};
+
+
+const updateCacheTrainer = () => {
+  const trainerNameJSON = JSON.stringify(PKM_TRAINER);
+  localStorage.setItem("name", trainerNameJSON);
+}
+
+const getCacheTrainer = () => {
+  trainerNameJSON = localStorage.getItem("name");
+  if (trainerNameJSON) {
+    PKM_TRAINER = JSON.parse(trainerNameJSON);
   }
-  
-  const getCache = () =>{
-    bagJSON=localStorage.getItem("bag");
-    if(bagJSON) {
-      BAG=JSON.parse(bagJSON);
-    }
-    listaBag();
+  askName()
+}
+
+
+const askName = () => {
+  if (PKM_TRAINER <= []){
+    namePokemonTrainer()
   }
+}
+
--- proyectoFinalJs/js/index.js
+++ proyectoFinalJs/js/index.js
@@ -1,23 +1,13 @@
 console.log("Gracias por usar nuestra pokedex");
 
-getCache();
-listaDePokemones();
-
-let generoEntrenadorPokemon = prompt("Ingrese genero (m o f): ").toLocaleLowerCase()
-while(generoEntrenadorPokemon != "m" && generoEntrenadorPokemon != "f") {
-    generoEntrenadorPokemon = prompt("Reingrese genero  con las letras 'm' o 'f': ").toLocaleLowerCase()
-}
-let nombreEntrenadorPokemon = prompt("Ingrese nombre: ")
-
-
-const holaF="Bienvenida entrenadora "+nombreEntrenadorPokemon;
-const holaM="Bienvenido entrenador "+nombreEntrenadorPokemon;
-//ternario
-generoEntrenadorPokemon =="m" ? alert(holaM) : alert(holaF);
-
-
-
-
 const btn5 = document.getElementById("btn5")
 const reload = document.getElementById('reload');
 btn5.addEventListener('click', _ => location.reload());
+
+getCache();
+listaDePokemones();
+
+askName();
+
+
+
